842d9f9871d9b629742b471057582cc4604348e5
Arthur Lockman <hello@rthr.me>
2016-10-30 11:56:02 -0400

    Added image server component.

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ app.use('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
+app.use('/self.jpg', function(req, res) {
+  res.sendFile(path.join(__dirname, '/self.jpg'));
+});
+
 app.listen(port, function() {
   console.log('App is listening on port ' + port);
 });
